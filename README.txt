800a03525481337c04df63879259bfeb0ad2d55a
Seah Ghim Sia <gsseah@yahoo.com>
2021-03-12 01:55:42 +0800

    New files

---
+++ README.txt
@@ -0,0 +1,28 @@
+Install Lumen Guide:
+https://lumen.laravel.com/docs/5.1
+-------------------------
+This project uses MySQL as the database.
+Create a new database [moolahdb] on your MySQL
+------------------------------
+Go to yourdirectory/owner
+Edit .env file:
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=moolahdb
+DB_USERNAME={yoursetting}
+DB_PASSWORD={yoursetting}
+------------------------
+To populate dummy data run:
+php artisan db:seed
+This will populate 3 rows of data. Use these referral codes for testing:
+1. ABC123
+2. DEF123
+3. GHI123
+------------------------
+To start your Lumen:
+php -S localhost:8000 -t public
+------------------------
+To access the form:
+http://localhost:8000/contact.php
+-----------------------
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+       //  $this->call('OwnerTableSeeder');
+	DB::table('owner_details')->insert([
+            'name' => 'John Doe',
+            'referral_code' => 'ABC123',
+        ]);
+
+        DB::table('owner_details')->insert([
+            'name' => 'Sue Chen',
+            'referral_code' => 'DEF123',
+        ]);
+
+        DB::table('owner_details')->insert([
+            'name' => 'Jack Doe',
+            'referral_code' => 'GHI123',
+        ]);
     }
 }
